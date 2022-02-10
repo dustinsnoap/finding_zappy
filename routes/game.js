b06f8e7ac2cd01bacfe7adff125cb99054b4adc6
dustin <dustinsnoap@gmail.com>
2022-02-10 13:06:30 -0500

    added user endpoint, some minor changes

--- routes/game.js
+++ routes/game.js
@@ -2,6 +2,7 @@ var Hash = require('md5')
 
 //cache
 const players = {}
+const customerPlayerMap = {}
 const zappyMap = {
     "zade1d": {"name": "common", "value": 1},
     "z48106": {"name": "uncommon", "value": 3},
@@ -21,13 +22,31 @@ module.exports = app => {
     app.post('/addZappy', addZappy)
     app.post('/addManyZappys', addManyZappys)
     app.get('/getAllZappys', getAllZappys)
+    app.get('/user', user)
+}
+
+const user = (req, res) => {
+    const customerId = req.headers.customerid
+    if(customerId in customerPlayerMap) {
+        const playerId = customerPlayerMap[customerId]
+        players[playerId].rank = Object.keys(players).sort((a, b) => players[b].points - players[a].points).indexOf(playerId)+1
+        const response = players[playerId]
+        res.status(200).send(response)
+        return
+    }
+    res.status(646).send("customerId not recgonized")
 }
 
 const join = (req, res) => {
     const playerName = req.body.name
     const zapposId = req.body.customerId
     const playerId = "p"+Hash(playerName + zapposId).substring(0,10)
+    if(playerId in players) {
+        res.status(677).send("Player name already in use")
+        return
+    }
     players[playerId] = {"name": playerName, "id": playerId, "startTime": new Date().getTime(), "points": 0, "rank": 99999, "collection": {}}
+    customerPlayerMap[zapposId] = playerId
 
     const response = players[playerId]
 
@@ -37,8 +56,12 @@ const join = (req, res) => {
 const stats = (req, res) => {
     const playerId = req.headers.playerid
     players[playerId].rank = Object.keys(players).sort((a, b) => players[b].points - players[a].points).indexOf(playerId)+1
-    const response = players[playerId]
-    res.status(200).send(response)
+    if(playerId in players) {
+        const response = players[playerId]
+        res.status(200).send(response)
+        return
+    }
+    res.status(646).send("playerId not found")
 }
 
 const find = (req, res) => {
@@ -72,7 +95,7 @@ const get = (req, res) => {
 }
 
 const top10 = (req, res) => {
-    const ranks = Object.keys(players).sort((a, b) => players[b].points - players[a].points).splice(0,10).map(id => { return {"name": players[id].name, "points": players[id].points}})
+    const ranks = Object.keys(players).sort((a, b) => players[b].points - players[a].points).splice(0,10).map(id => { return {"id": id, "name": players[id].name, "points": players[id].points}})
     res.status(200).send(ranks)
 }
 
@@ -81,7 +104,6 @@ const addZappy = (req, res) => {
     const zappyImgUrl = req.body.imgUrl
     const zappyValue = req.body.value
     const zappyId = req.body.zappyId ? req.body.zappyId : "z" + Hash(zappyName + zappyImgUrl + zappyValue).substring(0,9)
-    console.log(zappyId)
     if(zappyId in zappyMap) {res.status(648).send(`We have enough ${zappyName}'s`); return}
     zappyIds.push(zappyId)
     zappyMap[zappyId] = {"name": zappyName, "imgUrl": zappyImgUrl, "value": zappyValue}
